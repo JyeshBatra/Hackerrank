7001211beba2e0a8083cca93b3971002f4e94686
JyeshBatra <67318749+JyeshBatra@users.noreply.github.com>
2021-07-23 01:22:24 +0530

    Create inputAndOutput.cpp

---
+++ C++/inputAndOutput.cpp
@@ -0,0 +1,26 @@
+// Objective
+// In this challenge, we practice reading input from stdin and printing output to stdout.
+
+// Sample Input
+
+// 1 2 7
+// Sample Output
+
+// 10
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c;
+    cin>>a>>b>>c;
+    int sum=a+b+c;
+    cout<<sum; 
+    return 0;
+}
